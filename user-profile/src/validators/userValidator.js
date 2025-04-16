fd9f703563b56c983140102cb436785184e8e820
Hellsingi <il.dzemchanka@gmail.com>
2025-04-16 09:27:31 +0200

    feat: add docker

--- user-profile/src/validators/userValidator.js
+++ user-profile/src/validators/userValidator.js
@@ -1,6 +1,11 @@
 const Joi = require('joi');
 
-exports.userSchema = Joi.object({
+const userSchema = Joi.object({
     name: Joi.string().min(2).required(),
     email: Joi.string().email().required(),
+    age: Joi.number().optional(),
 });
+
+module.exports = {
+    userSchema
+};
